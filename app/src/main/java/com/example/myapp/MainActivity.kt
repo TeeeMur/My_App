558f985e27de5405cbe8f2c6cb7c131a5ee96f05
TeeeMur <98852194+TeeeMur@users.noreply.github.com>
2023-10-31 01:07:41 +0300

    Revert " >засунул кнопку и LazyVerticalGrid в Scaffold, кнопку положил в floatingActionButton"
    This reverts commit d3850437b1f5521af862e67f28996821f834ca66.

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -4,10 +4,13 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -16,8 +19,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -35,10 +36,11 @@ import androidx.compose.ui.unit.sp
 const val STR_NAME_SAVE_COUNT = "objCount"
 const val INT_COUNT_OF_COLUMNS_LANDSCAPE = 4
 const val INT_COUNT_OF_COLUMNS_PORTRAIT = 3
+const val FLOAT_SIZE_OF_COLUMNS_LANDSCAPE = 0.8f
+const val FLOAT_SIZE_OF_COLUMNS_PORTRAIT = 0.92f
 class MainActivity : ComponentActivity() {
 
 	private var rectCount = 0
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -46,31 +48,30 @@ class MainActivity : ComponentActivity() {
 		        rectCount = savedInstanceState.getInt(STR_NAME_SAVE_COUNT)
 	        }
 	        val countOfRectangles = remember{mutableStateOf(rectCount)}
-	        Scaffold(
-				floatingActionButton = {
-					Button(
-						onClick = {
-							rectCount++
-							countOfRectangles.value = rectCount
-						},
-						colors = ButtonDefaults.buttonColors(containerColor = Color.White),
-						elevation = ButtonDefaults.buttonElevation(
-							defaultElevation = 8.dp,
-							pressedElevation = 0.dp
-						)
-					) {
-						Text(
-							modifier = Modifier.padding(4.dp),
-							text = stringResource(R.string.button_name),
-							fontSize = 20.sp,
-							color = Color.Black
-						)
-					}
-				},
-		        containerColor = Color.Black
-			) {
-				innerPadding ->
-		        MyLazyVerticalGrid(countOfRectangles, innerPadding)
+	        Column(
+		        modifier = Modifier
+			        .fillMaxSize()
+			        .background(Color.Black)
+			        .padding(10.dp),
+		        horizontalAlignment = Alignment.CenterHorizontally,
+		        verticalArrangement = Arrangement.SpaceBetween
+	        ) {
+		        MyLazyVerticalGrid(countOfRectangles)
+		        Button(
+			        onClick = {
+				        rectCount++
+				        countOfRectangles.value = rectCount
+			        },
+			        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+			        modifier = Modifier
+				        .fillMaxWidth()
+		        ) {
+			        Text(
+				        text = stringResource(R.string.button_name),
+				        fontSize = 24.sp,
+				        color = Color.Black
+			        )
+		        }
 	        }
         }
     }
@@ -105,18 +106,17 @@ fun Rectangle(num: Int) {
 }
 
 @Composable
-fun MyLazyVerticalGrid(countOfBoxes: MutableState<Int>, padd: PaddingValues) {
+fun MyLazyVerticalGrid(countOfBoxes: MutableState<Int>) {
 	val screenConfig = LocalConfiguration.current
 	val countOfColumns =
 		(if (screenConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) INT_COUNT_OF_COLUMNS_LANDSCAPE else INT_COUNT_OF_COLUMNS_PORTRAIT)
+	val gridMaxHeight = (if (screenConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) FLOAT_SIZE_OF_COLUMNS_LANDSCAPE else FLOAT_SIZE_OF_COLUMNS_PORTRAIT)
 	LazyVerticalGrid(
 		columns = GridCells.Fixed(countOfColumns),
 		verticalArrangement = Arrangement.spacedBy(10.dp),
 		horizontalArrangement = Arrangement.spacedBy(10.dp),
-		contentPadding = PaddingValues(8.dp),
 		modifier = Modifier
-			.fillMaxSize()
-			.padding(padd)
+			.fillMaxHeight(gridMaxHeight)
 	){
 		items(countOfBoxes.value) {numOfBox ->
 			Rectangle(num = numOfBox)
